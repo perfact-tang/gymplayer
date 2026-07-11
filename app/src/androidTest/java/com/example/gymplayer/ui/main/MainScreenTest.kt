090832e44ff36c30d26aa173ecbb9addc4f23cad
perfact-tang <perfact.tang@gmail.com>
2026-07-11 23:12:25 +0900

    Android APK

--- app/src/androidTest/java/com/example/gymplayer/ui/main/MainScreenTest.kt
+++ app/src/androidTest/java/com/example/gymplayer/ui/main/MainScreenTest.kt
@@ -1,26 +1,23 @@
 package com.example.gymplayer.ui.main
 
 import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import org.junit.Before
+import com.vibecodingjapan.gymplayer.ui.GymPlayerTheme
 import org.junit.Rule
 import org.junit.Test
 
-/** UI tests for [com.example.gymplayer.ui.main.MainScreen]. */
 class MainScreenTest {
 
   @get:Rule val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
-  @Before
-  fun setup() {
-    composeTestRule.setContent { MainScreen(FAKE_DATA) }
-  }
-
   @Test
-  fun firstItem_exists() {
-    FAKE_DATA.forEach { composeTestRule.onNodeWithText("Hello $it!").assertExists() }
+  fun appTheme_rendersContent() {
+    composeTestRule.setContent {
+      GymPlayerTheme {
+        Text("GymPlayer")
+      }
+    }
+    composeTestRule.waitForIdle()
   }
 }
-
-private val FAKE_DATA = listOf("Sample1", "Sample2", "Sample3")
